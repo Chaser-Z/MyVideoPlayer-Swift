eeb3bd948a8bc54523ba0a6ad32f1feba021f105
Zhang Hainan <zhanghn@han-sky.com>
2017-04-25 11:24:44 +0800

    Initial Commit

---
+++ MyVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVideoPlayer
+//
+//  Created by 张海南 on 2017/4/25.
+//  Copyright © 2017年 Beijing Han-sky Education Technology Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
